c6f004611c86146c1b9a88fc432c6913a5106a55
kishore <csaikishore145@gmail.com>
2023-03-20 21:17:59 +0530

    rename cart1 to cart

--- ams/src/App.js
+++ ams/src/App.js
@@ -1,37 +1,36 @@
-
-import {Routes,Route} from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Product from './components/Product';
-import About from './components/About';
-import { ThemeProvider } from './components/themeContext';
-import BikeParts from './components/BikeParts'
-import Register from './components/Register';
-import Card from './components/Card'
-import Test from './components/Test'
-import NoPage from './components/NoPage';
-import Cart1 from './components/Cart1';
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Product from "./components/Product";
+import About from "./components/About";
+import { ThemeProvider } from "./components/themeContext";
+import BikeParts from "./components/BikeParts";
+import Register from "./components/Register";
+import Card from "./components/Card";
+import Test from "./components/Test";
+import NoPage from "./components/NoPage";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <>
-    <ThemeProvider>
-    <Nav />
-    <Routes>
-      <Route path="/"  element={<Home />} /> 
-      <Route path="login" element={<Login />} />
-      <Route path="bikes" element={<Product />} />
-      <Route path="about" element={<About />} />
-      <Route path="spareparts" element={<BikeParts />} /> 
-      <Route path="register" element={<Register />} /> 
-      <Route path="upload" element={<Card />} /> 
-      <Route path="test" element={<Test />} />
-      <Route path="cart1" element={<Cart1 />} />
-      <Route path="*" element={<NoPage />} />
-   </Routes>
-   </ThemeProvider>
-  </>
+      <ThemeProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="bikes" element={<Product />} />
+          <Route path="about" element={<About />} />
+          <Route path="spareparts" element={<BikeParts />} />
+          <Route path="register" element={<Register />} />
+          <Route path="upload" element={<Card />} />
+          <Route path="test" element={<Test />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
