9df33bbe48dd66bd6ee4b9956e71d91930fb375c
2100032410 <2100032410@kluniversity.in>
2023-03-20 23:34:06 +0530

    2nd

--- ams/src/components/Cart.js
+++ ams/src/components/Cart.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import axios from "axios";
 import { useState } from "react";
-
+import '../styles/cart.css';
 
 
 function Cart1() {
@@ -30,7 +30,7 @@ function Cart1() {
           return(
             <div>
     <section class="h-100" >
-    <div class="container h-100 py-5">
+    <div class="cart_container h-100 py-5">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-10">
   
@@ -43,11 +43,12 @@ function Cart1() {
             <div class="card-body p-4">
               <div class="row d-flex justify-content-between align-items-center">
                 <div class="col-md-2 col-lg-2 col-xl-2">
-                  <img
+                  <div class="k justify-content-between align-items-center"><img className="cart_img"
                     src={obj.image}alt="Cotton T-shirt"/>
+                    </div>
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-3">
-                  <p class="lead fw-normal mb-2">{obj.name}</p>
+                  <p class="lead fw-normal mb-4">{obj.name}</p>
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                   <button class="btn btn-link px-2"
@@ -72,22 +73,12 @@ function Cart1() {
               </div>
             </div>
           </div>
-  
-  
-          <div class="card mb-4">
-            <div class="card-body p-4 d-flex flex-row">
-              <div class="form-outline flex-fill">
-                <input type="text" id="form1" class="form-control form-control-lg" />
-                <label class="form-label" htmlFor="form1">Discound code</label>
-              </div>
-              <button type="button" class="btn btn-outline-warning btn-lg ms-3">Apply</button>
-            </div>
-          </div>
-  
-          <div class="card">
-            <div class="card-body">
-              <button type="button" class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>
-            </div>
+
+
+        
+            <div class="card-body justify-content-center align-items-center">
+              <button type="button" class="cart_btn btn-warning btn-block btn-lg ">Proceed to Pay</button>
+            
           </div>
   
         </div>
