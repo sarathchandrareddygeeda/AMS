f2d291826354ff91a89b5cfaa8f511448cc4a568
kishore <csaikishore145@gmail.com>
2023-05-06 16:01:49 +0530

    cart

--- ams/src/components/Cart.js
+++ ams/src/components/Cart.js
@@ -7,6 +7,7 @@ import { useTheme } from "./themeContext";
 function Cart1() {
   const [result, setResult] = useState(" ");
   const themer = useTheme();
+  const [sum,setSum]=useState(0)
   useEffect(() => {
     getProducts1();
   });
@@ -52,17 +53,17 @@ function Cart1() {
     return (
       <div>
         <div class="d-flex justify-content-between align-items-center mb-4">
-          <h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>
+          <h3 class="k1  align-items-center text-black ">Shopping Cart</h3>
         </div>
         {result.map((obj) => {
           if (obj.email === themer.email) {
             return (
               <div>
                 <section class="h-100">
-                  <div class="cart_container h-100 py-5">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                      <div class="col-10">
-                        <div class="card rounded-3 mb-4">
+                  <div class="cart_container h-100 py-5 ">
+                    <div class="row d-flex justify-content-center align-items-center h-100 card-body">
+                      <div class="col-10 border border-dark">
+                        <div class=" mb-4">
                           <div class="card-body p-4">
                             <div class="row d-flex justify-content-between align-items-center">
                               <div class="col-md-2 col-lg-2 col-xl-2">
@@ -77,24 +78,8 @@ function Cart1() {
                               <div class="col-md-3 col-lg-3 col-xl-3">
                                 <p class="lead fw-normal mb-4">{obj.name}</p>
                               </div>
-                              <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                <button class="btn btn-link px-2">
-                                  <i class="fas fa-minus"></i>
-                                </button>
-                                <input
-                                  id="form1"
-                                  min="0"
-                                  name="quantity"
-                                  value="2"
-                                  type="number"
-                                  class="form-control form-control-sm"
-                                />
-                                <button class="btn btn-link px-2">
-                                  <i class="fas fa-plus"></i>
-                                </button>
-                              </div>
-                              <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                <h5 class="mb-0">{obj.price}</h5>
+                              <div class="col-md-3 col-lg-3 col-xl-2 offset-lg-1">
+                                <h5 class="mb-0">₹{obj.price}</h5>
                               </div>
                               <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                                 <a href="#!" class="text-danger">
@@ -102,6 +87,7 @@ function Cart1() {
                                   <button class="btn btn-primary" type="button" onClick={() => handleDelete(obj._id)}>
                                   Remove
                                   </button>
+                        
                                   </i>
                                   </a>
                               </div>
@@ -114,12 +100,11 @@ function Cart1() {
                 </section>
               </div>
             );
-          } else {
-            return <div>no items</div>;
-          }
+          } 
         })}
       </div>
     );
   }
+
 }
 export default Cart1;
